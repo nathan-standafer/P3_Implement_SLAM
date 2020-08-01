019e29882472f7718252ebb5d177ab7a468c52d5
Nathan Standafer <standafer_na@yahoo.com>
2020-08-01 09:19:51 -0400

    implemented sense function

--- robot_class.py
+++ robot_class.py
@@ -80,15 +80,31 @@ class robot:
         measurements = []
         
         ## TODO: iterate through all of the landmarks in a world
-        
+        for index, landmark in enumerate(self.landmarks):
+            
         ## TODO: For each landmark
         ## 1. compute dx and dy, the distances between the robot and the landmark
+            dx = landmark[0] - self.x
+            dy = landmark[1] - self.y
         ## 2. account for measurement noise by *adding* a noise component to dx and dy
         ##    - The noise component should be a random value between [-1.0, 1.0)*measurement_noise
         ##    - Feel free to use the function self.rand() to help calculate this noise component
+        ##    - It may help to reference the `move` function for noise calculation
+            dx += self.rand() * measurement_noise
+            dy += self.rand() * measurement_noise
+        
         ## 3. If either of the distances, dx or dy, fall outside of the internal var, measurement_range
         ##    then we cannot record them; if they do fall in the range, then add them to the measurements list
         ##    as list.append([index, dx, dy]), this format is important for data creation done later
+            x = self.x + dx 
+            y = self.y + dy 
+
+            if x < 0.0 or x > self.world_size or y < 0.0 or y > self.world_size:
+                #outside range.  do nothing
+                pass
+            else:
+                measurements.append([index, dx, dy])
+        
         
         ## TODO: return the final, complete list of measurements
         return measurements
